a09a92c637144062e3374210e93be4453a508d4d
sonamirijanyan <57571008+sonamirijanyan@users.noreply.github.com>
2019-11-12 21:26:59 +0400

    updated version

--- myUtils.js
+++ myUtils.js
@@ -1,19 +1,22 @@
 module.exports = {
-	isPrime: function(n) {
-		if(n <=1) {
-			return false;
-		}
+    isPrime: function (n) {
+        if (n <= 1) {
+            return false;
+        }
 
-		let div = 2;
+        let div = 2;
 
-		while(div <= Math.sqrt(n)) {
-			if(n % div === 0) {
-				return false;
-			}
-
-			div++;
-		}
-
-		return true;
-	}
+        while (div <= Math.sqrt(n)) {
+            if (n % div === 0) {
+                return false;
+            }
+            div++;
+        }
+        return true;
+    },
+    printArray: function (array) {
+        for ( i=0; i<array.length; i++){
+            console.log(array[i]);
+        }
+    }
 }
