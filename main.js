b073439d769f735a02fd54ff2bdb4d805bb712b1
sonamirijanyan <57571008+sonamirijanyan@users.noreply.github.com>
2019-11-12 21:25:52 +0400

    updated version of main.js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const util = require("./myUtils.js");
+const reader = require("readline-sync");
+let a =  reader.questionInt("Please, insert your first number = ");
+let b = reader.questionInt("Please, insert your second number = ");
 
-console.log(util.isPrime(2));
 
+if (a>0 && b>0){
+    let primeNumbers=[];
+    for(let i=a; i<=b; i++){
+        if (util.isPrime(i)){
+            primeNumbers.push(i);}
+    }
+    util.printArray(primeNumbers);
+} else{
+    console.log("input positive integers.");
+}
